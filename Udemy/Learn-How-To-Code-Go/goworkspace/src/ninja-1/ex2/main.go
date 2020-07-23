51a19d4e7102f2f7fedee0131047c162262b6492
your name <desaiadityapr@gmail.com>
2020-07-23 18:14:52 +0530

    Ninja leve 1 completed

---
+++ Udemy/Learn-How-To-Code-Go/goworkspace/src/ninja-1/ex2/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	// compiler will assign the zero values of that type to the variables defined
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
